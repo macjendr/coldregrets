f3946666b7a8fbb76f47e85102250247bd265880
macjendr <macjendr@gmail.com>
2018-06-18 00:19:31 +0200

    second scene added + transition, beacon with fire animation

---
+++ scenes/Message.gd
@@ -0,0 +1,32 @@
+extends RichTextLabel
+
+# Variables
+var dialog = [
+	"Everything was ok. Then the storm came…",
+	""]
+var bb_format = [
+"[font=res://fonts/roboto_regular.tres][color=#FFFFFF]",
+"[/color][/font]"
+]
+var page = 0
+
+# Functions
+func _ready():
+	set_process_input(true)
+	set_bbcode(bb_format[0] + dialog[page] + bb_format[1])
+	set_visible_characters(0)
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog.size()-1:
+				page += 1
+				set_bbcode(bb_format[0] + dialog[page] + bb_format[1])
+				set_visible_characters(0)
+			else:
+				get_tree().change_scene("res://fonts/roboto_regular.tres")
+		else:
+			set_visible_characters(get_total_character_count())
+
+func _on_Timer_timeout():
+	set_visible_characters(get_visible_characters()+1)
---
+++ scenes/RichTextLabel.gd
@@ -0,0 +1,32 @@
+extends RichTextLabel
+
+# Variables
+var dialog = [
+	"Everything was ok.",
+	"Then the storm came…"]
+var bb_format = [
+"[center][font=res://fonts/indie_flower.tres][color=#000000]",
+"[/color][/font][/center]"
+]
+var page = 0
+
+# Functions
+func _ready():
+	set_process_input(true)
+	set_bbcode(bb_format[0] + dialog[page] + bb_format[1])
+	set_visible_characters(0)
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept"):
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog.size()-1:
+				page += 1
+				set_bbcode(bb_format[0] + dialog[page] + bb_format[1])
+				set_visible_characters(0)
+			else:
+				get_tree().change_scene("res://scenes/StartScene.tscn")
+		else:
+			set_visible_characters(get_total_character_count())
+
+func _on_Timer_timeout():
+	set_visible_characters(get_visible_characters()+1)
